bb58f7601bb303aea91a12982644e4977c7270f6
azaibMeer <azaibmir1292@gmail.com>
2023-09-03 23:48:13 +0500

    top product and purchase query
    top product and purchase query

--- app/Http/Controllers/Admin/DashbordController.php
+++ app/Http/Controllers/Admin/DashbordController.php
@@ -26,6 +26,23 @@ class DashbordController extends Controller
         WHERE MONTH(created_at) = MONTH(CURRENT_DATE())
         AND YEAR(created_at) = YEAR(CURRENT_DATE())'); 
         
+        $data['top_products_sale'] =  DB::select('SELECT name,main_image, SUM(order_detail.qty)
+                            AS total_product_sales FROM products 
+                            INNER JOIN order_detail 
+                            ON products.id = order_detail.product_id
+                            GROUP BY products.id, products.name,products.main_image
+                            ORDER BY total_product_sales DESC
+                            LIMIT 3');
+    
+        $data['top_purchsed_customers'] =  DB::select('SELECT name,profile, COUNT(order_detail.user_id) 
+                            AS top_purchasing_user FROM order_detail 
+                            INNER JOIN users 
+                            ON users.id = order_detail.user_id
+                            WHERE order_detail.status = 3
+                            GROUP BY users.id, users.name,users.profile
+                            ORDER BY top_purchasing_user DESC
+                            LIMIT 3');   
+
         return view('admin.layouts.dashboard',$data);   
     }
     /**
